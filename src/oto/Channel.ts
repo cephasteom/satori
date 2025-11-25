459fd1b4bef2b2bc05e0142ebf2b45c7a823c89b
Peter Thomas <petethomasmusique@gmail.com>
2025-11-25 21:21:05 +0000

    add ct-synths

--- src/oto/Channel.ts
+++ src/oto/Channel.ts
@@ -7,30 +7,43 @@ destination.channelCount = destination.maxChannelCount // set to max channels
 const output = new Merge({channels: destination.maxChannelCount}) // create output merger
 output.connect(destination) // connect to system audio output
 
+const synthTypes = [
+    'synth', 'sampler', 'granular', 'additive', 'acid', 'drone', 'sub', 'superfm', 'wavetable', 
+    'zmod', 
+    'tone.synth', 'tone.mono', 'tone.fm', 'tone.am'
+];
+
+const console = new BroadcastChannel('sartori');
+
 export class Channel {
-    input: Gain
-    _out: number
-    _fx: any
-    _reverb: any
-    _delay: any
-    _fader: Gain
-    _output
+    out: number // output channel index
+    input: Gain // input gain
+    output: Split // output splitter
+    fader: Gain // volume control
+    instruments: Record<string, any> = {}
     
     constructor(out: number = 0) {
-        this._out = out
+        this.out = out
 
         this.input = new Gain(1)
-        this._fader = new Gain(1)
-        this._output = new Split({channels: 2})
+        this.fader = new Gain(1)
+        this.output = new Split({channels: 2})
         
-        this._fader.connect(this._output)
-        this.input.fan(this._fader)
+        this.fader.connect(this.output)
+        this.input.fan(this.fader)
         
-        this._output.connect(output, 0, out)
-        this._output.connect(output, 1, out+1)
+        this.output.connect(output, 0, out)
+        this.output.connect(output, 1, out+1)
     }
 
     play(params: any, time: number) {
-        console.log('play channel', params, time)
+        const { inst } = params;
+        
+        if(!synthTypes.includes(inst)) {
+            return console.postMessage({ 
+                type: 'error', 
+                message: `Instrument type "${inst}" not recognized.` 
+            });
+        }
     }
 }
