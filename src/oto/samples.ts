6dfbeba6e0b8a749f57c2f24b7b46ed1b06b219a
Peter Thomas <petethomasmusique@gmail.com>
2025-12-08 16:47:26 +0000

    fix samples()

--- src/oto/samples.ts
+++ src/oto/samples.ts
@@ -22,12 +22,21 @@ const result = repos.map(url =>
         .then((json: Record<string, Array<string>>) => {
             if(!json) return
             
-            return Object.entries(json)
+            const samples = Object.entries(json)
                 .filter(([bank]) => bank !== '_base')
                 .reduce((obj, [bank, samples]: [string, Array<string>]) => ({
                     ...obj,
                     [bank]: [samples].flat().map((sample: string) => `${json._base}${sample}`)
                 }), {} as Record<string, Array<string>>);
+
+            if(Object.keys(samples).length > 0) {
+                sartori.postMessage({ type: 'success', message: 'Sample banks ->\n' });
+                sartori.postMessage({ type: 'info', message: Object.keys(samples).join(',\n') });
+            } else {
+                sartori.postMessage({ type: 'warning', message: 'No sample banks loaded' });
+            }
+
+            return samples;
         })
         .catch(_ => sartori.postMessage({ type: 'error', message: `Couldn't load samples from ${url}` }))
     )
@@ -38,10 +47,3 @@ const result = repos.map(url =>
     }, Promise.resolve({} as Record<string, Array<string>>));
 
 export const samples = await result || {};
-
-if(Object.keys(samples).length > 0) {
-    sartori.postMessage({ type: 'success', message: 'Sample banks ->\n' });
-    sartori.postMessage({ type: 'info', message: Object.keys(samples).join(',\n') });
-} else {
-    sartori.postMessage({ type: 'warning', message: 'No sample banks loaded' });
-}
